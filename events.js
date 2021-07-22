2e0e64b62207d24350f885ce9aa770c24903c1bc
Dan St. Aubin <staubindj@gmail.com>
2021-07-22 13:51:18 -0500

    adding in events.js

--- events.js
+++ events.js
@@ -6,11 +6,22 @@ function onReady() {
     console.log('so ready!');
 
     // set up event listener
+    // event handler
+    // event registration
+    // all equivalent terms, broadly
     $('#addBtn').on('click', addToList);
 
 
 }
 
 function addToList() {
-    
+    console.log('button got clicked!');
+    let inputArea = $('#aboutYou')
+    let userInput = inputArea.val();
+    if (userInput.length > 0) {
+        $('ul').append(`<li>
+                            User Input: ${userInput}
+                        </li>`);
+        inputArea.val('');
+    }
 }
