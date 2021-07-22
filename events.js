b99ac53bc34191e21ccd39a0251ac61785370fbb
Dan St. Aubin <staubindj@gmail.com>
2021-07-22 15:01:09 -0500

    class thru 3pm. added delete button to lists

--- events.js
+++ events.js
@@ -5,12 +5,23 @@ $(document).ready(onReady);
 function onReady() {
     console.log('so ready!');
 
+    // Handle add to list button events
     // set up event listener
     // event handler
     // event registration
     // all equivalent terms, broadly
     $('#addBtn').on('click', addToList);
+    // or
+    // $(document).on('click', '#addBtn', addToList);
 
+
+    // Handle Delete button events
+    // won't work because deleteBtn doesn't exist yet
+    // console.log($('.deleteBtn'))
+    // $('.deleteBtn').on('click', deleteItem);
+
+    // "Event delegation"
+    $(document).on('click', '.deleteBtn', deleteItem);
 
 }
 
@@ -19,9 +30,16 @@ function addToList() {
     let inputArea = $('#aboutYou')
     let userInput = inputArea.val();
     if (userInput.length > 0) {
-        $('ul').append(`<li>
-                            User Input: ${userInput}
-                        </li>`);
+        $('#outputArea').append(`<li>
+                                    User Input: ${userInput}
+                                    <button class="deleteBtn">Delete Me</button>
+                                </li>`);
         inputArea.val('');
     }
+}
+
+function deleteItem() {
+    // $(this).remove()
+    console.log('in delete item');
+    $(this).parent().remove();
 }
